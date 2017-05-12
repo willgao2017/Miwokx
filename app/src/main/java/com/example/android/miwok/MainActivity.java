475ab217272553ea44d3bb72f4435b8c98ef864e
will <will.gao2017@outlook.com>
2017-05-12 14:47:08 +0800

    separate files/code to use java to realize on-click reaction

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -21,6 +21,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import static android.R.attr.button;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -29,11 +31,12 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        NumbersClickListener clickListener = new NumbersClickListener();
+
+        TextView numbers = (TextView)findViewById(R.id.numbers);
+        numbers.setOnClickListener(clickListener);
     }
 
-    public void openNumbersList(View view) {
-        Intent i = new Intent(this, NumbersActivity.class);
-        startActivity(i);
-    }
 
 }
